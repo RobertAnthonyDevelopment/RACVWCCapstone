1ae3ce981005d14a9d82e2d1cb87ffd0cabbd81c
RobertAnthonyDevelopment <156968669+RobertAnthonyDevelopment@users.noreply.github.com>
2024-08-11 17:00:41 -0700

    Create app.js
    Migration from Codepen @RobertAnthonyDevelopment - August 11th 2024

---
+++ app.js
@@ -0,0 +1,5 @@
+// script.js
+function showMessage() {
+  document.getElementById("message").innerHTML =
+    "''If There is No Struggle, There is No Progress.''</br> Frederick Douglass, Speech on West India Emancipation, August 3, 1857";
+}
